56a8d52a008f2d2a08958d30ff326825e07ce768
Nikola Pleša <nickplesha@gmail.com>
2016-01-27 18:49:27 +0100

    Initial commit

---
+++ dist/3dtouch.js
@@ -0,0 +1,146 @@
+// install   :      cordova plugin add https://github.com/EddyVerbruggen/cordova-plugin-3dtouch.git
+// link      :      https://github.com/EddyVerbruggen/cordova-plugin-3dtouch
+
+(function() {
+    'use strict';
+
+    angular.module('ngCordova.plugins.3dtouch', [])
+        .factory('$cordova3DTouch', cordova3DTouch);
+
+    function cordova3DTouch($q) {
+        var quickActions = [];
+        var quickActionHandler = {};
+
+        var createQuickActionHandler = function(quickActionHandler) {
+            return function (payload) {
+                for (var key in quickActionHandler) {
+                    if (payload.type === key) {
+                        quickActionHandler[key]();
+                    };
+                };
+            };
+        };
+
+        return {
+            /*
+             * Checks if Cordova 3D touch is present and loaded
+             *
+             * @return   promise
+             */
+            isAvailable: function () {
+                var deferred = $q.defer();
+                if (!window.cordova) {
+                    deferred.reject('Not supported in browser');
+                } else {
+                    if (!ThreeDeeTouch) {
+                        deferred.reject('Could not find 3D touch plugin')
+                    } else {
+                        ThreeDeeTouch.isAvailable(function (value) {
+                            deferred.resolve(value);
+                        }, function (err) {
+                            deferred.reject(err);
+                        });
+                    }
+                }
+
+                return deferred.promise;
+            },
+
+            /*
+             * Add a quick action to menu
+             *
+             * @param    string type
+             * @param    string title
+             * @param    string iconType (optional)
+             * @param    string subtitle (optional)
+             * @param    function callback (optional)
+             * @return   promise
+             */
+            addQuickAction: function(type, title, iconType, subtitle, callback) {
+                var deferred = $q.defer();
+
+                this.isAvailable().then(function() {
+                    quickActions.push({
+                        type,
+                        title,
+                        subtitle,
+                        iconType
+                    });
+                    quickActionHandler[type] = callback;
+                    ThreeDeeTouch.configureQuickActions(quickActions);
+                    ThreeDeeTouch.onHomeIconPressed = createQuickActionHandler(quickActionHandler);
+                    deferred.resolve(quickActions);
+                },
+                function(err) {
+                    deferred.reject(err);
+                });
+
+                return deferred.promise;
+            },
+
+            /*
+             * Add a quick action handler. Used for static quick actions
+             *
+             * @param    string type
+             * @param    function callback
+             * @return   promise
+             */
+            addQuickActionHandler: function(type, callback) {
+                var deferred = $q.defer();
+
+                this.isAvailable().then(function() {
+                    quickActionHandler[type] = callback;
+                    ThreeDeeTouch.onHomeIconPressed = createQuickActionHandler(quickActionHandler);
+                    deferred.resolve(true);
+                },
+                function(err) {
+                    deferred.reject(err);
+                });
+
+                return deferred.promise;
+            },
+
+            /*
+             * Enable link preview popup when force touch is appled to link elements
+             *
+             * @return   bool
+             */
+            enableLinkPreview: function() {
+                var deferred = $q.defer();
+
+                this.isAvailable().then(function() {
+                    ThreeDeeTouch.enableLinkPreview();
+                        deferred.resolve(true);
+                },
+                function(err) {
+                    deferred.reject(err);
+                });
+
+                return deferred.promise;
+            },
+
+            /*
+             * Add a hanlder function for force touch events,
+             *
+             * @param    function callback
+             * @return   promise
+             */
+            addForceTouchHandler: function(callback) {
+                var deferred = $q.defer();
+
+                this.isAvailable().then(function() {
+                    ThreeDeeTouch.watchForceTouches(callback);
+                    deferred.resolve(true);
+                },
+                function(err) {
+                    deferred.reject(err);
+                });
+
+                return deferred.promise;
+            }
+        };
+    };
+
+    cordova3DTouch.$inject = ['$q'];
+
+})();
